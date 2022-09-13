1a83322472c39c32673cfd1c47f41d715025f90d
daniel.bistox <daniel@bistox.com>
2022-09-13 13:58:59 -0400

    fix: styles reference and implementation

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import i18nEN from "./i18n/en";
-import "./style.module.css";
+import Css from "./style.module.css";
 
 const Header: React.FC = () => {
   return (
     <Head>
-      <title>{i18nEN.title}</title>
+      <title className={Css["title"]}>{i18nEN.title}</title>
       <meta
         name="description"
         content="Rick and Morty characters api exercise"
