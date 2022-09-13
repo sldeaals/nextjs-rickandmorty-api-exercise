3cf4f0f560bf8fba102334077bc0c530d011c40b
daniel.bistox <daniel@bistox.com>
2022-09-13 13:58:59 -0400

    fix: styles reference and implementation

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
-import "./style.module.css";
+import Css from "./style.module.css";
 import i18nEN from "./i18n/en";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <a
+        className={Css["link"]}
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
       >
         {i18nEN.poweredBy}
-        <span className="logo">
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <span className={Css["logo"]}>
+        <Image className={Css["image"]} src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
       </a>
     </footer>
